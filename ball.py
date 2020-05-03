a219bdf3800eee13647648c264d3ad59d37023f6
alexbulavin2009 <64378278+alexbulavin2009@users.noreply.github.com>
2020-05-03 12:54:50 -0400

    Update ball.py

--- ball.py
+++ ball.py
@@ -6,8 +6,21 @@ class Ball:
         self.canvas = canvas
         self.id = canvas.create_oval(10, 10, 25, 25, fill = color)
         self.canvas.move(self.id, 245, 100)
+        self.x = 1
+        self.y = -1
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width() 
     def drow(self):
-        self.canvas.move(self.id, 0, -1)
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+        if pos[1] <= 0:
+            self.y = 1
+        if pos[3] >= self.canvas_height:
+            self.y = -1
+        if pos[0] <= 0:
+            self.x = 1
+        if pos[2] >= self.canvas_width:
+            self.x = -1    
 tk = Tk()
 tk.title('game')
 tk.resizable(0, 0)
@@ -21,4 +34,3 @@ while 1:
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
-   
